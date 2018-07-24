24b39f7b76d3c698e59c6109164556b373f0e9c1
AL_ON_PC <alon.berrios@gmail.com>
2018-07-24 13:50:25 -0400

    Ajustes menores en footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,7 +12,7 @@ export class FooterComponent {
   public anio:number = new Date().getFullYear();
 
   public direccion:string = "Compañía #2540 Oficina 103, Santiago";
-  public correo:string = "mda.arq@ gmail.com";
+  public correo:string = "mda.arq@ gmail.com"; //Un espacio intencional, para que el estilo lo tome, sino no aplica responsive
   public telefono:number = 26825253;
 
 }
